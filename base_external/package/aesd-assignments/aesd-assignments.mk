10f530818a59984ca172eb467eaa34aac69a4c6c
Tony Kim <tkim_weston@yahoo.com>
2024-01-09 23:56:50 -0500

    Assignment 4 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'f8f57554cd66774d6de017431a6686eddbfdd10e'
+AESD_ASSIGNMENTS_VERSION = 'fcfdbeb55cf076d958be468006ed812c64f41ae6'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
