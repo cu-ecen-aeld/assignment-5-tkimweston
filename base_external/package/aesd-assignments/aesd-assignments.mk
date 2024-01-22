ebd3caba8afc3b2f642dbec5822d47c9fde4abac
Tony Kim <tkim_weston@yahoo.com>
2024-01-21 23:20:30 -0500

    assignment 5 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,16 +7,18 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = 'fcfdbeb55cf076d958be468006ed812c64f41ae6'
-AESD_ASSIGNMENTS_VERSION = '56acde24843cf080b308a5c39e6cd606b33ffc5c'
+AESD_ASSIGNMENTS_VERSION = '3c34a5ae2fe649c2828226f540888e3f8c4eb4b3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-tkimweston.git'
+#AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-5-tkimweston.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server clean
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
